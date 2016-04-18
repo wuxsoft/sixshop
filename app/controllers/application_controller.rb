af44bf2002c7f550f4d7b1ef74f88a7869972cb0
wuxsoft <wuxsoft@gmail.com>
2016-04-18 20:32:17 +0800

    edit menu and panel menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,20 @@ class ApplicationController < ActionController::Base
     end    
     root_path
   end
+
+  def render_list(opts = {})
+    list = []
+    yield(list)
+    items = []
+    list.each do |link|
+      item_class = EMPTY_STRING
+      urls = link.match(/href=(["'])(.*?)(\1)/) || []
+      url = urls.length > 2 ? urls[2] : nil
+      if url && current_page?(url) || (@current && @current.include?(url))
+        item_class = 'active'
+      end
+      items << content_tag('li', raw(link), class: item_class)
+    end
+    content_tag('ul', raw(items.join(EMPTY_STRING)), opts)
+  end
 end
