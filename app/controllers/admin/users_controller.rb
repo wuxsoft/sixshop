3a104946c5245d906f094b1bf079dd3f48c165eb
Jason Lee <huacnlee@gmail.com>
2016-04-28 15:57:13 +0800

    Rewrite admin/users_controller to use Rails, simple_form default behaviors.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,17 +12,10 @@ def index
   end
 
   def create
-    @olduser =  User.where("email = ?",params[:user][:email])
-    if @olduser.count > 0
-        redirect_to(new_admin_user_path, alert: 'Is Exist User email.')
-        return
-    end
-
     @user = User.new(params[:user].permit!)
     @user.email = params[:user][:email]
-    @user.encrypted_password  = '88888888'
-    @user.updated_at = Time.new
-    @user.created_at = Time.new
+    @user.password = '88888888'
+    @user.password_confirmation  = '88888888'
     if @user.save
       redirect_to(admin_users_path, notice: 'User was successfully created.')
     else
@@ -40,40 +33,17 @@ def index
   end
 
   def update
-    if(params[:id]==1)
-      redirect_to admin_users_path
-      return 
-    end 
-    role_ids = params[:roles_id]
-    if (role_ids)
-      return 
-    end
-    @olduser =  User.where("email = ? AND id <> ?", params[:email], params[:d])
-    if @olduser.count > 0
-        redirect_to(edit_admin_user_path, alert: 'Is Exist User email.')
-        return
-    end
-
     @user = User.find(params[:id])
-    @user.email = params[:email]
-    @user.updated_at = Time.new
-    #roles = Role.where(id:role_ids)
-    #@user.roles = roles
-    #@user.roles.concat(roles)
-    @user.role_ids  = Role.where(id: params[:role_ids]).pluck(:id)
-    if @user.update_attributes(params[:user].permit! )
-        redirect_to(admin_users_path, notice: 'User was successfully updated.')
-      else
-        redirect_to admin_users_path
+    if @user.update_attributes(params[:user].permit!)
+      redirect_to(admin_users_path, notice: 'User was successfully updated.')
+    else
+      redirect_to admin_users_path
     end
   end
 
   def destroy
-    if(params[:id]==1)
-      redirect_to admin_users_path
-      return 
-    end
     @user = User.find(params[:id])
+
     if @user.destroy
       redirect_to(admin_users_path, notice: 'User was successfully deleted.')
     else
